1a9b7ec86a6a8642fdf38301be87f38f7f4162c7
Angel <angel.36b@gmail.com>
2023-04-30 20:45:29 -0500

    organizacion de lista de peliculas, titulo, año y poster

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 
 import './App.css'
+import responseMovies from './mocks/with-result.json'
+//import withoutResult from '.mocks/not-result.json'
+
 
 function App() {
+
+const movies = responseMovies.Search
+const hasMovies = movies?.length > 0
+
   return (
     <div className='page' >
       <header>
@@ -13,6 +20,30 @@ function App() {
       </header>
       <main>
         Aqui iran los resultados
+
+        {
+          hasMovies
+            ?(
+              <ul>
+                {
+                  movies.map(movie => (
+                    <li key={movie.imdbID}>
+                      <h3>{movie.Title}</h3>
+                      <p>{movie.Year}</p>
+                      <img src={movie.Poster} alt={movie.Title} />
+
+                    </li>
+                  ))
+                }
+              </ul>
+            ) 
+            :(
+          <p>No se encontraron peliculas para esta busqueda </p>
+          )
+            
+          
+            
+        }
       </main>
 
     </div>
